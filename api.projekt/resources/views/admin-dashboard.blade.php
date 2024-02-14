49191891c5d20f50c21ca8457a0a9d277a3f0dea
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-02-14 16:27:08 +0100

    Nu kanske

--- api.projekt/resources/views/admin-dashboard.blade.php
+++ api.projekt/resources/views/admin-dashboard.blade.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <div>
     <h1>Hello darkness my old friend.</h1>
 </div>
+</body>
+</html>
+
